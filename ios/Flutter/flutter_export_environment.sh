0bb0edd74d4841a0efb530d35bc6570f0b68844f
OmarHamed553 <74277257+OmarHamed553@users.noreply.github.com>
2021-08-31 00:08:54 +0800

    Added the new maps key in the manifest file

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Fathiiii\Delivery\flutter_application"
+export "FLUTTER_APPLICATION_PATH=D:\Work\Wekala\Delivery\flutter_application\wekalaDriver"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
